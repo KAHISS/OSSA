e732ffc096d3b3949f9b552237a06bce136a3254
Hamilton Freitas <hamilton.freitas@hotmail.com.br>
2026-04-11 09:09:38 -0300

    style: tela admin-dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import Sidebar from '@/components/ui/Sidebar';
+import TopBar from "@/components/ui/TopBar";
 import Header from '@/components/ui/Header';
 import SummaryCards from '@/components/ui/SummaryCards';
 import PendingPaymentsTable from '@/components/ui/PendingPaymentsTable';
@@ -7,10 +8,15 @@ export default function DashboardPage() {
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-1 p-6 bg-gray-100">
-        <Header />
-        <SummaryCards totalPendente={1250} totalRecebido={4500} />
-        <PendingPaymentsTable />
+
+      <main className="flex-1 bg-gray-100 min-h-screen">
+        <TopBar />
+
+        <div className="p-6">
+          <Header />
+          <SummaryCards totalPendente={1250} totalRecebido={4500} alunosAtrasados={8} alunosAtivos={120}/>
+          <PendingPaymentsTable />
+        </div>
       </main>
     </div>
   );
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,16 +1,11 @@
 export default function Header() {
   return (
-    <header className="bg-white p-6 shadow flex justify-between items-center">
-      <input
-        type="text"
-        placeholder="Pesquisar..."
-        className="border px-3 py-2 rounded w-1/3"
-      />
+    <header className="bg-zinc-900 p-6 shadow flex justify-between items-center">      
       <div className="text-right">
-        <h2 className="text-xl font-semibold">
+        <h2 className="text-xl text-white font-semibold">
           Bem-vindo de volta, <span className="text-red-600">Admin!</span>
         </h2>
-        <p className="text-sm text-gray-600">
+        <p className="text-sm text-white">
           Aqui está o resumo das atividades da sua academia.
         </p>
       </div>
