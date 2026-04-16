2806ba6548bf83eb77783ba55fe797450286e23f
Vinícius <viniciusandradebc@gmail.com>
2026-04-16 13:26:29 -0300

    feat: faz a listagem de usuários cadastrados no banco de dados

--- src/app/painel/usuarios/page.tsx
+++ src/app/painel/usuarios/page.tsx
@@ -16,10 +16,11 @@ import {
 } from "@/components/ui/hover-card";
 import { FaUsers, FaUserPlus } from 'react-icons/fa';
 import { Bebas_Neue, Oswald } from 'next/font/google';
+import { GET } from "@/app/api/usuarios/route";
 
 const oswald = Oswald({
     subsets: ['latin'],
-    weight: ['300', '500'],
+    weight: ['400', '500', '700'],
 });
 
 const bebas = Bebas_Neue({
@@ -27,96 +28,80 @@ const bebas = Bebas_Neue({
     weight: ['400'],
 });
 
-export default function UsersPage() {
+interface User {
+    id: string | number;
+    name: string;
+    email: string;
+    phone?: string;
+    weight?: number;
+    type?: string;
+    status?: string;
+}
+
+export default async function UsersPage() {
+    const users = await (await GET()).json();
+
     return (
         <div className={`my-10 mx-10 font-thin ${oswald.className}`}>
-            <div className="flex justify-between items-center mb-8">
+            <div className="flex flex-wrap items-center justify-between gap-4 mb-8">
                 <h1 className={`text-5xl ${bebas.className} flex items-center gap-3`}>
                     <FaUsers className="text-red-700" />
                     Gestão de Usuários
                 </h1>
-                <Button size="sm" className="bg-teal-800 hover:bg-teal-900 text-xl h-10 px-6">
-                    Cadastrar Usuário
-                </Button>
+
+                <div className="w-full sm:w-auto flex justify-center">
+                    <Button size="sm" className=" hover:bg-teal-900 text-xl h-10 px-6">
+                        Cadastrar Usuário
+                    </Button>
+                </div>
             </div>
 
-            <Table className="text-base">
-                <TableHeader>
-                    <TableRow className="font-normal text-[20px]">
-                        <TableHead className="w-[100px]">Nome</TableHead>
-                        <TableHead>Email</TableHead>
-                        <TableHead>Telefone</TableHead>
-                        <TableHead>Peso</TableHead>
-                        <TableHead>Tipo</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead className="text-right">Ação</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableCell className="max-w-[150px]">
-                            <HoverCard>
-                                <HoverCardTrigger asChild>
-                                    <div className="truncate cursor-help font-medium">
-                                        João de Santo Cristo Silva Sauro Terceiro
-                                    </div>
-                                </HoverCardTrigger>
-                                <HoverCardContent className="w-80">
-                                    <h1>Nome Completo: </h1>
-                                    <p>João de Santo Cristo Silva Sauro Terceiro</p>
-                                </HoverCardContent>
-                            </HoverCard>
-                        </TableCell>
-                        <TableCell>joao.silva@example.com</TableCell>
-                        <TableCell>+55 11 97777-7777</TableCell>
-                        <TableCell>82Kg</TableCell>
-                        <TableCell>Administrador</TableCell>
-                        <TableCell>Ativo</TableCell>
-                        <TableCell className="flex justify-end gap-2 py-4">
-                            <Button size="sm" variant="outline" className="text-[18px] border-blue-500 text-blue-500 hover:bg-blue-50 h-10 w-20">Editar</Button>
-                            <Button size="sm" className="text-[18px] bg-red-600 hover:bg-red-700 text-white h-10 w-20">Excluir</Button>
-                        </TableCell>
-                    </TableRow>
-
-                    <TableRow>
-                        <TableCell className="font-medium">Maria Oliveira</TableCell>
-                        <TableCell>maria.oliveira@example.com</TableCell>
-                        <TableCell>+55 21 98888-8888</TableCell>
-                        <TableCell>68Kg</TableCell>
-                        <TableCell>Aluno(a)</TableCell>
-                        <TableCell>Pendente</TableCell>
-                        <TableCell className="flex justify-end gap-2 py-4">
-                            <Button size="sm" variant="outline" className="text-[18px] border-blue-500 text-blue-500 hover:bg-blue-50 h-10 w-20">Editar</Button>
-                            <Button size="sm" className="text-[18px] bg-red-600 hover:bg-red-700 text-white h-10 w-20">Excluir</Button>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell className="font-medium">Carlos Souza</TableCell>
-                        <TableCell>carlos.souza@example.com</TableCell>
-                        <TableCell>+55 31 99999-9999</TableCell>
-                        <TableCell>90Kg</TableCell>
-                        <TableCell>Aluno(a)</TableCell>
-                        <TableCell>Suspenso</TableCell>
-                        <TableCell className="flex justify-end gap-2 py-4">
-                            <Button size="sm" variant="outline" className="text-[18px] border-blue-500 text-blue-500 hover:bg-blue-50 h-10 w-20">Editar</Button>
-                            <Button size="sm" className="text-[18px] bg-red-600 hover:bg-red-700 text-white h-10 w-20">Excluir</Button>
-                        </TableCell>
-                    </TableRow>
-
-                    <TableRow>
-                        <TableCell className="font-medium">Ana Pereira</TableCell>
-                        <TableCell>ana.pereira@example.com</TableCell>
-                        <TableCell>+55 41 96666-6666</TableCell>
-                        <TableCell>55Kg</TableCell>
-                        <TableCell>Instrutor(a)</TableCell>
-                        <TableCell>Inativo</TableCell>
-                        <TableCell className="flex justify-end gap-2 py-4">
-                            <Button size="sm" variant="outline" className="text-[18px] border-blue-500 text-blue-500 hover:bg-blue-50 h-10 w-20">Editar</Button>
-                            <Button size="sm" className="text-[18px] bg-red-600 hover:bg-red-700 text-white h-10 w-20">Excluir</Button>
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+            {users &&
+                <Table className="text-base">
+                    <TableHeader>
+                        <TableRow className="font-bold text-[20px]">
+                            <TableHead className="w-[100px]">Usuário</TableHead>
+                            <TableHead>Email</TableHead>
+                            <TableHead>Telefone</TableHead>
+                            <TableHead>Peso</TableHead>
+                            <TableHead>Tipo</TableHead>
+                            <TableHead className="text-right">Ação</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {users.map((user: User) => (
+                            <TableRow key={user.id}>
+                                <TableCell className="max-w-[150px]">
+                                    <HoverCard>
+                                        <HoverCardTrigger asChild className="truncate cursor-help font-bold">
+                                            <div>
+                                                {user.name}
+                                            </div>
+                                        </HoverCardTrigger>
+                                        <HoverCardContent className="w-80">
+                                            <h1>Nome Completo: </h1>
+                                            <p>{user.name}</p>
+                                        </HoverCardContent>
+                                    </HoverCard>
+                                </TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>{user.phone}</TableCell>
+                                <TableCell>{user.weight}Kg</TableCell>
+                                <TableCell>
+                                    {user.type === 'Student'
+                                        ? 'Aluno'
+                                        : user.type === 'Instructor'
+                                            ? 'Instrutor'
+                                            : 'Administrador'}
+                                </TableCell>
+                                <TableCell className="flex justify-end gap-2 py-4">
+                                    <Button size="sm" variant="outline" className="text-[18px] text-black hover:bg-red-50 hover:border-red-500 h-10 w-20">Editar</Button>
+                                    <Button size="sm" className="text-[18px] bg-red-600 hover:bg-red-700 text-white h-10 w-20">Excluir</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>}
         </div>
     );
 }
